7bbe1f52e05ceaf4fa4cd9eef7e7c6269feccf2b
PSADMIN <psadmin@psadmins-air-8.attlocal.net>
2021-05-16 19:56:02 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+// for loop for counting numbers from 10 to 
+// for(declare our variable;condition to be checked;work to be done after the loop is executed){work to be done if the condition is true
+//
+//}
+//for(var counter = 10; counter  >= 1; counter -- ){
+    //console.log(counter)
+//}
+
+// for loop counting numbers from 0 to 10
+//for(var counter = 0; counter <=10; counter ++){
+//console.log(counter)
+//}
+
+
+//while loops do something condition is true
+// while loop that prints from 0 to 10
+//var counter = 0;
+
+//while(counter <=10){
+//console.log(counter)
+//counter ++
+//}
+//for loop 1-5 print message on 5
+//for(counter = 1;counter <= 5; counter ++){
+   // console.log(counter)
+   // if(counter == 5){
+     //   console.log(counter + "counter =5 ")
+   // }
+//}
+// fizzbuzz challenge
+// print numbersfrom 1-100
+//for(numbers=1; numbers <=100; numbers ++){
+   // if both print fizzbuzz
+   // if(numbers % 3 == 0 && numbers % 5 == 0 ){
+   //     console.log(numbers, "fizzbuzz")
+    
+   //  }  // if number is divisible by 3 print fizz
+    // else if (numbers % 3 == 0){
+     //   console.log(numbers, "Fizz")}
+        // if numbers divisible by 5 print buzz
+      //  else if (numbers % 5 == 0){
+      //      console.log(numbers, "Buzz")
+            
+      //      }
+        // print other numbers
+     //   else {
+      //      console.log(numbers)
+      //  }
+
+    //}         
+    
+// loop through an array
+var colors = ["red", "black","yellow", "blue", "white"]
+for(counter = 0; counter <= colors.length -1; counter++)
+{
+    console.log(colors[counter])
+    if (counter == 3){
+        console.log(colors[counter], "this is the third item")
+    }
+
+}
+            
